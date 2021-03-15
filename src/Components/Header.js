09032bd099745b1d5cbfd0b1981ed6ba4a5e9138
Victor Breno <victor.brns2@gmail.com>
2021-03-15 19:50:09 -0300

    correçao lint

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 function Header() {
   return (
     <h1 className="header">StarWars Planets</h1>
-  )
+  );
 }
 
 export default Header;
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,45 +1,46 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import planetsContext from '../Hooks/planetsContext';
 
 function Table() {
   const { data, isFetching } = useContext(planetsContext);
-  let tableHeaders = []
+  let tableHeaders = [];
   if (data[0]) {
     tableHeaders = Object.keys(data[0]);
   }
 
-
   function renderTbody() {
     return (
       <tbody>
-        {data.map((obj) => {
-          return <tr>
-            {Object.entries(obj).map((values,index) => {
+        {data.map((obj, index) => (
+          <tr key={ index }>
+            {Object.entries(obj).map((values) => {
               if (values[0] !== 'residents') {
-                return <td key={index}>{values[1]}</td>
+                return (<td key={ index }>{values[1]}</td>);
               }
+              return null;
             })}
           </tr>
-        })}
+        ))}
       </tbody>
-    )
+    );
   }
 
   return (
     <table className="table">
       <thead>
         <tr>
-          {!isFetching ?
-            tableHeaders.map((header) => {
+          {!isFetching
+            ? tableHeaders.map((header) => {
               if (header !== 'residents') {
-                return <th key={header} scope="col">{header}</th>
+                return <th key={ header } scope="col">{header}</th>;
               }
-          }) : <span>Carregando...</span>}
+              return null;
+            }) : <span>Carregando...</span>}
         </tr>
       </thead>
-        {!isFetching ? renderTbody() : <span>Carregando...</span>}
+      {!isFetching ? renderTbody() : <span>Carregando...</span>}
     </table>
-  )
+  );
 }
 
 export default Table;
