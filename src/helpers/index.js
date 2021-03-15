b8d7ec4904ad1b2eccd9ebbe00781d1c67019d42
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-03-15 17:53:08 -0300

    Troca o fetch de todas as paginas por apenas na primeira pagina

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,14 +1,21 @@
-export default async function fetchAllPlanets(url = 'https://swapi-trybe.herokuapp.com/api/planets/', actualList = []) {
-  const allPlanets = await fetch(url)
+// export default async function fetchAllPlanets(url = 'https://swapi-trybe.herokuapp.com/api/planets/', actualList = []) {
+//   const allPlanets = await fetch(url)
+//     .then((data) => data.json())
+//     .then((data) => {
+//       if (data.results) {
+//         actualList = [...actualList, ...data.results];
+//       }
+//       if (data.next) {
+//         return fetchAllPlanets(data.next, actualList);
+//       }
+//       return actualList;
+//     });
+//   return allPlanets;
+// }
+
+export default async function fetchAllPlanets() {
+  const allPlanets = await fetch('https://swapi-trybe.herokuapp.com/api/planets/')
     .then((data) => data.json())
-    .then((data) => {
-      if (data.results) {
-        actualList = [...actualList, ...data.results];
-      }
-      if (data.next) {
-        return fetchAllPlanets(data.next, actualList);
-      }
-      return actualList;
-    });
+    .then((data) => data.results);
   return allPlanets;
 }
