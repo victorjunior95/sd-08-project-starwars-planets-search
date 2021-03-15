5879dba091997c18eb21c01535486af3497074fb
rafasysop <rafaelcostamoura@gmail.com>
2021-03-15 14:57:13 -0300

    requisito 01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import buscaPlanetas from './api';
+import StarWarsContext from './context/StarWarsContext';
+import Table from './components/Table';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    buscaPlanetas()
+      .then((resp) => setData(resp));
+  }, []);
   return (
-    <span>Hello, App!</span>
+    <StarWarsContext.Provider value={ data }>
+      <span>Hello, App!</span>
+      <Table />
+    </StarWarsContext.Provider>
   );
 }
 
--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@ const BASE_URL = 'https://swapi-trybe.herokuapp.com/api/planets/';
 
 const buscaPlanetas = () => fetch(BASE_URL)
   .then((res) => res.json())
-  .then((data) => console.log(data.results));
+  .then((data) => data.results);
 
 export default buscaPlanetas;
