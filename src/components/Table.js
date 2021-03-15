1b5acc639cc3a307cf621c75875ae8d7696385ec
Aline Debastiani <debastianialine@gmail.com>
2021-03-15 20:35:48 -0300

    cumpre requisito 1

---
+++ src/components/Table.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+
+function Table() {
+  const [data, setData] = useState([]);
+
+  const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+  useEffect(() => {
+    const fetchUrl = async () => {
+      const { results } = await fetch(url).then((response) => response.json());
+      setData(results);
+    };
+    fetchUrl();
+  }, []);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        { data.map((planet) => (
+          <tr key={ planet.name }>
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>{planet.films}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.url}</td>
+          </tr>
+        )) }
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
