ed59415c81e9c02f6e38a618b1e3789da5c978d0
rafasysop <rafaelcostamoura@gmail.com>
2021-03-15 16:06:58 -0300

    Requisito 02

--- src/App.js
+++ src/App.js
@@ -3,19 +3,48 @@ import './App.css';
 import buscaPlanetas from './api';
 import StarWarsContext from './context/StarWarsContext';
 import Table from './components/Table';
+import Filters from './context/Filters';
+
+const INITIAL_FILTER = {
+  filters: {
+    filterByName: {
+      name: '',
+    },
+  },
+};
 
 function App() {
   const [data, setData] = useState([]);
+  const [filters, setFilters] = useState(INITIAL_FILTER);
+  const [dataf, setDataf] = useState([]);
 
   useEffect(() => {
     buscaPlanetas()
-      .then((resp) => setData(resp));
+      .then((resp) => setDataf(resp));
   }, []);
+
+  useEffect(() => {
+    let filteredData = dataf;
+    const { filters: { filterByName: { name } } } = filters;
+    if (dataf !== [] && name !== '') {
+      filteredData = filteredData.filter((item) => item.name.includes(name));
+    }
+    setData(filteredData);
+  }, [dataf, filters]);
+
+  function setFilter({ target }) {
+    setFilters({
+      ...filters, filters: { filterByName: { name: target.value } },
+    });
+  }
   return (
-    <StarWarsContext.Provider value={ data }>
-      <span>Hello, App!</span>
-      <Table />
-    </StarWarsContext.Provider>
+    <Filters.Provider value={ [filters, setFilter] }>
+      <StarWarsContext.Provider value={ data }>
+        <span>Hello, App!</span>
+        <input onChange={ setFilter } placeholder="Buscar Nome" data-testid="name-filter" />
+        <Table />
+      </StarWarsContext.Provider>
+    </Filters.Provider>
   );
 }
 
