293873a5177ae3413428e626c45e673236ebbda7
rafasysop <rafaelcostamoura@gmail.com>
2021-03-15 14:13:07 -0300

    Iniciando Projeto

---
+++ src/api.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://swapi-trybe.herokuapp.com/api/planets/'
+
+const buscaPlanetas = async () => {
+  return await fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => console.log(data.results));
+}
+
+buscaPlanetas();
