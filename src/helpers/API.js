a829fab04c76b4755f126805d270280053388145
hugobrr <hugobrr@gmail.com>
2021-03-15 17:08:57 -0300

    tabela feita

--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,7 +1,8 @@
 const STARWARS_API = 'https://swapi-trybe.herokuapp.com/api/planets/';
-async function fetchStarWars() {
+async function FetchStarWars() {
   const response = await fetch(STARWARS_API);
-  const planets = response.await.response.json();
-  return planets;
+  const planets = await response.json();
+  console.table(planets.results);
+  return planets.results;
 }
-export default fetchStarWars;
+export default FetchStarWars();
--- src/pages/table.js
+++ src/pages/table.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function table() {
+const STARWARS_API = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+export default function Table() {
+  const [ApiDATA, setApiData] = useState([]);
+
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await fetch(STARWARS_API);
+        const DATA = await response.json();
+        setApiData(DATA.results);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    getApi();
+  }, []);
+
+  const tableHeader = ['name', 'rotation_period', 'orbital_period', 'diameter', 'climate',
+    'gravity', 'terrain', 'surface_water', 'population', 'residents', 'films', 'created',
+    'edited', 'url'];
+
   return (
     <div>
-      test
+      <table>
+        <tr>
+          {tableHeader.map((item) => (
+            <td key={ item.value }>
+              {item}
+            </td>
+          ))}
+        </tr>
+        {ApiDATA.map((line) => (
+          <tr key={ line.name }>
+            {tableHeader.map((item) => (<td key={ item.value }>{line[item]}</td>))}
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
