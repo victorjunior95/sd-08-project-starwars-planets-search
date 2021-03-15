43bcef8b4c947b28a6c04a9731a8523c4c94a95b
Seu nome <luiza.dilly@gmail.com>
2021-03-15 16:02:57 -0300

    REQ 1

---
+++ src/context/Provider.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import FilterContext from './FilterContext';
+
+function Provider({ children }) {
+  const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { results } = await fetch(url).then((response) => response.json());
+      // console.log(results);
+      setPlanets(results);
+    }
+    fetchData();
+  }, []);
+
+  const data = {
+    planets,
+    setPlanets,
+  };
+
+  return (
+    <FilterContext.Provider value={ data }>
+      { children }
+    </FilterContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
+
+export default Provider;
