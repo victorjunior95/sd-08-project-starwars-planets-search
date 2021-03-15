55bb5e4f57de1fb0d19626e2d40a5d9fd269b12d
vivianeflowt <vivianeflowt@gmail.com>
2021-03-15 18:18:02 -0300

    update...

--- src/components/PlanetsTable.jsx
+++ src/components/PlanetsTable.jsx
@@ -21,33 +21,33 @@ const TABLE_HEADERS = [
 
 function PlanetsTable({ data }) {
   const builder = () => data.map((i, index) => (
-    <tr role="row" key={ index }>
-      <th>{i.name}</th>
-      <th>{i.rotation_period}</th>
-      <th>{i.orbital_period}</th>
-      <th>{i.diameter}</th>
-      <th>{i.climate}</th>
-      <th>{i.gravity}</th>
-      <th>{i.terrain}</th>
-      <th>{i.surface_water}</th>
-      <th>{i.population}</th>
-      <th>{i.films}</th>
-      <th>{i.created}</th>
-      <th>{i.edited}</th>
-      <th>{i.url}</th>
+    <tr key={ index }>
+      <td>{i.name}</td>
+      <td>{i.rotation_period}</td>
+      <td>{i.orbital_period}</td>
+      <td>{i.diameter}</td>
+      <td>{i.climate}</td>
+      <td>{i.gravity}</td>
+      <td>{i.terrain}</td>
+      <td>{i.surface_water}</td>
+      <td>{i.population}</td>
+      <td>{i.films}</td>
+      <td>{i.created}</td>
+      <td>{i.edited}</td>
+      <td>{i.url}</td>
     </tr>));
 
   return (
     <table>
-      <tbody>
-        <tr role="row">
+      <thead>
+        <tr>
           { TABLE_HEADERS.map((i, index) => (
-            <th key={ index } role="columnheader">
+            <th key={ index }>
               {i}
             </th>)) }
         </tr>
-        { builder()}
-      </tbody>
+      </thead>
+      { builder()}
     </table>
   );
 }
