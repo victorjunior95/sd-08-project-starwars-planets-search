3a7beb371135d20eca646eb3225ffa8f3a0973e5
patriciavsilva <silva.patricia.v@gmail.com>
2021-03-15 23:06:14 -0300

    Iniciando o Projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import PlanetsProvider from './Context/PlanetsProvider';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <PlanetsProvider>
+
+      <p>Patrícia</p>
+
+    </PlanetsProvider>
   );
 }
 
