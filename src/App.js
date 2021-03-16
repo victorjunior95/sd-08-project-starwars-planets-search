67e5d54014e63c5466d9b3fdc3c97fc7dbd25c54
Tandy <tandyfw@gmail.com>
2021-03-16 13:39:46 -0300

    Resolvido problema, passando Provider para o App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import React, { useContext, useEffect } from 'react';
-import Context from './context';
+import React from 'react';
+import Provider from './context/Provider';
 import './App.css';
 import Table from './component/Table';
 import NameFilter from './component/NameFilter';
 
 function App() {
-  const { setData, setHeaders, setFilteredData } = useContext(Context);
-  useEffect(() => {
-    const fetchPlanets = async () => {
-      const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
-      const { results } = await fetch(endpoint).then((res) => res.json());
-      setData(results);
-      const headers = Object.keys(results[0]).filter((head) => head !== 'residents');
-      setHeaders(headers);
-      setFilteredData(results);
-    };
-    fetchPlanets();
-  }, [setData, setHeaders, setFilteredData]);
   return (
-    <main>
-      <NameFilter />
-      <Table />
-    </main>
+    <Provider>
+      <main>
+        <NameFilter />
+        <Table />
+      </main>
+    </Provider>
   );
 }
 
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Context from './index';
 
@@ -17,7 +17,24 @@ function Provider({ children }) {
     filter,
     setFilter,
     filteredData,
-    setFilteredData };
+    setFilteredData,
+  };
+
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets/';
+      const { results } = await fetch(endpoint).then((res) => res.json());
+      const formatData = results.reduce((planets, next) => {
+        delete next.residents;
+        return [...planets, next];
+      }, []);
+      setData(formatData);
+      setHeaders(Object.keys(formatData[0]));
+      setFilteredData(formatData);
+    };
+    fetchPlanets();
+  }, [setData, setHeaders, setFilteredData]);
+
   return (
     <Context.Provider value={ context }>
       { children }
