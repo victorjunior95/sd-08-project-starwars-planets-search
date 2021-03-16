edd6ddd2faf4006b65ad774985c36fbb1c92d280
adrianWarrior <adrianoforcellini>
2021-03-16 19:12:35 -0300

    5 deveria funcionar levar bug ao plantao. função funciona no browser mas nao na aplicação

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
 import StarContext from '../context/context';
-import { objetosEmComum, objetosEmComum2, objetosEmComum3, objetosEmComum4 } from './HyperFilter';
+// import { objetosEmComum, objetosEmComum2, objetosEmComum3, objetosEmComum4 } from './HyperFilter';
 
 export default function Table() {
   const valorInicialDeComparação = 0;
@@ -16,8 +16,88 @@ export default function Table() {
   const [arrayOfNumericFilter, setArrayOfNumericFilter] = useState([]);
 
   const filterName = (array) => {
-    const arrTdFiltered = array.filter((obj) => obj.name.includes((input)));
+    const arrTdFiltered = array.filter((obj) => obj.name.includes(input));
     return arrTdFiltered;
+  };
+  const objetosEmComum = (ARRAY) => {
+    const inicioDaContagemDeItens = 0;
+    let maiorArray = [];
+    if (ARRAY.length === 1) {
+      return ARRAY[0];
+    }
+    for (let i = inicioDaContagemDeItens; i < ARRAY.length; i += 1) {
+      if (ARRAY[i].length > maiorArray.length) {
+        maiorArray = ARRAY[i];
+      }
+    }
+    return maiorArray;
+  };
+  // essa primeira parte pegou o maior array dentro do arrayzao e pos numa variavel
+
+  const objetosEmComum2 = (ARRAY) => {
+    const arr = [...ARRAY];
+    const inicioDaContagemDeItens = 0;
+    const maiorArray = objetosEmComum(ARRAY);
+    for (let i = inicioDaContagemDeItens; i < arr.length; i += 1) {
+      if (arr[i] === maiorArray) {
+        arr.splice(i, 1);
+      }
+    }
+    // essa segunda parte removeu o maior array de dentro do arrayzao...
+    return arr;
+  };
+
+  const objetosEmComum3 = (ARRAY) => {
+    const inicioDaContagemDeItens = 0;
+    const ARRAYY = objetosEmComum2(ARRAY);
+    const maiorArray = objetosEmComum(ARRAY);
+    const arrFinal = [];
+    for (let i = inicioDaContagemDeItens; i < maiorArray.length; i += 1) {
+      for (let l = inicioDaContagemDeItens; l < ARRAYY.length; l += 1) {
+        for (let j = inicioDaContagemDeItens; j < ARRAYY[l].length; j += 1) {
+          if (JSON.stringify(maiorArray[i]) === JSON.stringify(ARRAY[l][j])
+           && ARRAYY[l][j] !== undefined) {
+            arrFinal.push(ARRAYY[l][j]);
+          }
+        }
+      }
+    }
+
+    return arrFinal;
+  };
+
+  const objetosEmComum4 = (ARRAY) => {
+    if (ARRAY.length === 1) {
+      return ARRAY[0];
+    }
+    const inicioDaContagemDeItens = 0;
+    const arrFinal = objetosEmComum3(ARRAY);
+    const ARRAYY = objetosEmComum2(ARRAY);
+    const organizado = [];
+    for (let i = inicioDaContagemDeItens; i < arrFinal.length; i += 1) {
+      organizado.push(JSON.stringify(arrFinal[i]));
+    }
+    organizado.sort();
+    const arrDeSaida = [];
+    let count = 1;
+    for (let i = inicioDaContagemDeItens; i < organizado.length; i += 1) {
+      if (organizado[i] === organizado[i + 1]) {
+        count += 1;
+        if (count === ARRAYY.length) {
+          arrDeSaida.push(JSON.parse(organizado[i]));
+          count = 1;
+        }
+      } else if (ARRAYY.length === 1) {
+        arrDeSaida.push(JSON.parse(organizado[i]));
+      }
+    }
+    for (let i = 0; i < ARRAYY.length; i += 0) {
+      if (ARRAYY[i] === []) {
+        return ARRAYY[i];
+      }
+    }
+
+    return arrDeSaida;
   };
 
   const filterNumber = () => {
@@ -46,6 +126,13 @@ export default function Table() {
     setArrayOfNumericFilter(arr);
   };
 
+  const existeArrayOfNumeric = () => {
+    if (arrayOfNumericFilter[1] !== undefined) {
+      alert('entrounoif, reconheceu arrayOfNumericFilter');
+      return objetosEmComum4(arrayOfNumericFilter);
+    }
+    return arrayTd;
+  };
   return (
     <div className="back">
       {' '}
@@ -98,7 +185,7 @@ export default function Table() {
         <button
           type="button"
           data-testid="button-filter"
-          onClick={ () => console.log(arrayOfNumericFilter) }
+          onClick={ () => console.log('a') }
         >
           Consolearray
         </button>
@@ -121,7 +208,7 @@ export default function Table() {
         </thead>
         <tbody>
           {/* objetosEmComum4(arrayOfNumericFilter)) */}
-          {filterName(arrayTd).map((obj) => (
+          {filterName(existeArrayOfNumeric()).map((obj) => (
             <tr className="table" key={ obj.name }>
               <td className="table">{obj.name}</td>
               <td className="table">{obj.terrain}</td>
