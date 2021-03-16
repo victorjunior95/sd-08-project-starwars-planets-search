5c280cdc05f89b6c187aaf941cc99ff43a625570
Joao <joaocarlitoneto@gmail.com>
2021-03-16 12:09:18 -0300

    Requisito 2 Concluido - Corrigindo eslint tentativa 1

---
+++ src/components/SearchBar.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import ContextApi from '../context/Context';
+
+function SearchBar() {
+  const { filterByName, setFilterName } = useContext(ContextApi);
+  return (
+    <label htmlFor="search">
+      Filtrar:
+      <input
+        type="text"
+        name="search"
+        id="search"
+        data-testid="name-filter"
+        value={ filterByName }
+        onChange={ (ev) => setFilterName(ev.target.value) }
+      />
+    </label>
+  );
+}
+
+export default SearchBar;
