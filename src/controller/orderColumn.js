07bff5db4dba19e749fab501dc18061b9beee9b0
Ediberto Oliveira <edibertooliveira@aol.com>
2021-03-17 13:35:33 -0300

    fix:  refatory order number

--- src/controller/orderColumn.js
+++ src/controller/orderColumn.js
@@ -1,11 +1,18 @@
 const SORT_ORDER_ASC = 1;
 const SORT_ORDER_DESC = -1;
+const IGNORE_COLUMNS_WITH_NUMBERS = ['orbital_period',
+  'rotation_period',
+  'diameter',
+  'surface_water',
+  'population'];
+
+const noNumberColumn = (value) => !IGNORE_COLUMNS_WITH_NUMBERS.includes(value);
 
 export const sortOrderAsc = (a, b, column) => {
   const columnOrderA = a[column];
   const columnOrderB = b[column];
 
-  if (column !== 'orbital_period') {
+  if (noNumberColumn(column)) {
     if (columnOrderA > columnOrderB) return SORT_ORDER_ASC;
     if (columnOrderA < columnOrderB) return SORT_ORDER_DESC;
   }
@@ -16,7 +23,7 @@ export const sortOrderDesc = (a, b, column) => {
   const columnOrderA = a[column];
   const columnOrderB = b[column];
 
-  if (column !== 'orbital_period') {
+  if (noNumberColumn(column)) {
     if (columnOrderA < columnOrderB) return SORT_ORDER_ASC;
     if (columnOrderA > columnOrderB) return SORT_ORDER_DESC;
   }
