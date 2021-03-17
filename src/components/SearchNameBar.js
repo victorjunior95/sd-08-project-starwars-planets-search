6fec9cdf265b9ce8193793c86a9042f58c9f74d6
Leonardo <lecastro16@gmail.com>
2021-03-17 19:23:54 -0300

    iniciando o requisito 3 com problema no avaliador

--- src/components/SearchNameBar.js
+++ src/components/SearchNameBar.js
@@ -3,6 +3,16 @@ import StarWarsContext from '../context/StarWarsContext';
 
 function SearchNameBar() {
   const { setName } = useContext(StarWarsContext);
+  const columnFilter = [
+    '',
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
+
+  const comparisonFilter = ['', 'Maior que', 'Menor que', 'Igual a'];
 
   return (
     <div>
@@ -13,6 +23,43 @@ function SearchNameBar() {
           type="text"
           name="searchName"
           onChange={ (e) => setName(e.target.value) }
+        />
+      </label>
+      <label htmlFor="selectFilter">
+        Selecione o filtro:
+        <select
+          name="selectFilter"
+          data-testid="column-filter"
+          onChange={ (e) => console.log(e.target.value) }
+        >
+          {columnFilter.map((element) => (
+            <option value={ element } key={ element }>
+              { element }
+            </option>
+          ))}
+        </select>
+      </label>
+      <label htmlFor="selectComparison">
+        Selecione a comparação
+        <select
+          name="selectComparison"
+          data-testid="comparison-filter"
+          onChange={ (e) => console.log(e.target.value) }
+        >
+          {comparisonFilter.map((element) => (
+            <option key={ element } value={ element }>
+              { element }
+            </option>
+          ))}
+        </select>
+      </label>
+      <label htmlFor="numberFilter">
+        Digite o número:
+        <input
+          data-testid="value-filter"
+          type="number"
+          name="numberFilter"
+          onChange={ (e) => console.log(e.target.value) }
         />
       </label>
     </div>
