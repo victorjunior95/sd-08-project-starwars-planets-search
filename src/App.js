1a9ac1077d09eabcc20dbaec4bcb83d9578e99f2
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-03-18 17:38:40 -0300

    Requisito 01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Context from './components/Context';
+import Table from './components/Table';
 
 function App() {
+  const [planets, setPlanets] = useState({});
+  const [isLoad, setIsLoad] = useState(true);
+  useEffect(() => {
+    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+      .then((response) => response.json())
+      .then((data) => {
+        setPlanets(data.results);
+        setIsLoad(false);
+      });
+  }, []);
   return (
-    <span>Hello, App!</span>
+    <div>
+      {isLoad
+        ? (<p>Carregando</p>)
+        : (
+          <Context.Provider value={ planets }>
+            <Table />
+          </Context.Provider>
+        )}
+    </div>
   );
 }
 
