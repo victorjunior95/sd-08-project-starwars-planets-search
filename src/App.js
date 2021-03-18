9723898c739aae889d2dbaa85a1bb0e4328f5ccc
Clênio Galvão <clenio.cgm@gmail.com>
2021-03-18 17:29:44 -0300

    Resolve quesito 6 bonus

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import PlanetsProvider from './context/PlanetsProvider';
 import PlanetsTable from './components/planetsTable';
 import './App.css';
 import Filtros from './components/filtros';
+import Order from './components/order';
 
 function App() {
   return (
     <PlanetsProvider>
       <Filtros />
+      <Order />
       <PlanetsTable />
     </PlanetsProvider>
   );
