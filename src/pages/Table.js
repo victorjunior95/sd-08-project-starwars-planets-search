45b2ce3033df8458e24f136e9c9463259e0b5d81
Leonardo <lecastro16@gmail.com>
2021-03-18 19:19:04 -0300

    requisito 3 v3

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -10,8 +10,21 @@ function Table() {
     isFetching,
     filters: {
       filterByName: { name },
+      filterByNumericValues,
     },
   } = useContext(StarWarsContext);
+  
+  if(filterByNumericValues.length>0) {
+    const result2 = filterByNumericValues.map((filtered)=> data.filter((element)=>element[filtered.column]>filtered.value)
+    );
+
+    console.log(filterByNumericValues);
+    // console.log(data[0]);
+    // console.log(filterByNumericValues[0].value);
+    const newData = data.filter((element)=>element[filterByNumericValues[0].column]>filterByNumericValues[0].value)
+    // console.log(newData)
+  }
+  
 
   const filteredData = data
     .filter((value) => value.name.toLowerCase().includes(name.toLowerCase()));
