7e4041b04c6cc3d590aa13e80fc11047222b0f3c
Daniel Leon <daniel.radialista@gmail.com>
2021-03-18 19:08:13 -0300

    requisito 1, ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Context from './components/Context';
+import Table from './components/Table';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+  useEffect(() => {
+    const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setPlanets(data.results);
+        setisLoading(false);
+      });
+  }, []);
   return (
-    <span>Hello, App!</span>
+    <div>
+      { isLoading ? 'Loading...'
+        : (
+          <Context.Provider value={ planets }>
+            <Table />
+          </Context.Provider>
+        )}
+    </div>
+
   );
 }
 
