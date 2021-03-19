6283a437955e2b9db74834764c43da85d2a20e96
wagnerberna <cabelo29007@gmail.com>
2021-03-19 10:22:37 -0300

    req01

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,12 +3,47 @@ import ContextStarWars from '../Context/ContextStarWars';
 
 function Table() {
   const { planets } = useContext(ContextStarWars);
-  console.log(planets);
+  console.log(planets[0]);
   return (
-    <div>
-      <h1>Tabela</h1>
-      {/* <span>{planets[0].name}</span> */}
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        {planets.map((el) => (
+          <tr key={ el.name }>
+            <td>{el.name}</td>
+            <td>{el.rotation_period}</td>
+            <td>{el.orbital_period}</td>
+            <td>{el.diameter}</td>
+            <td>{el.climate}</td>
+            <td>{el.gravity}</td>
+            <td>{el.terrain}</td>
+            <td>{el.surface_water}</td>
+            <td>{el.population}</td>
+            <td>{el.films}</td>
+            <td>{el.created}</td>
+            <td>{el.edited}</td>
+            <td>{el.url}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+
   );
 }
 
