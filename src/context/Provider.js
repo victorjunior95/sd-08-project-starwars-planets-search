482fff6c4b2c3e8dd5a68d802ee7eea91db49224
Víctor Cançado <victor.cancado@gmail.com>
2021-03-20 03:20:02 -0300

    Req. 1corrigido

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import StarWarsContext from './StarWarsContext';
 import getPlanets from '../services/index';
 
