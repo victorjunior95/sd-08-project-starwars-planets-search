692f5a2775392c11efb92b9568476f02eeb7f623
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2021-03-21 19:55:01 -0300

    th ou td

--- src/components/Table.js
+++ src/components/Table.js
@@ -38,24 +38,23 @@ function Table() {
             return planets.name.toLowerCase().includes(fill.toLowerCase());
           }).map((planet, index) => (
             <tr key={ index }>
-              <th>{ planet.name }</th>
-              <th>{ planet.rotation_period }</th>
-              <th>{ planet.orbital_period }</th>
-              <th>{ planet.diameter }</th>
-              <th>{ planet.climate }</th>
-              <th>{ planet.gravity }</th>
-              <th>{ planet.terrain }</th>
-              <th>{ planet.surface_water }</th>
-              <th>{ planet.population }</th>
-              <th>{ planet.films }</th>
-              <th>{ planet.created }</th>
-              <th>{ planet.edited }</th>
-              <th>{ planet.url }</th>
+              <td>{ planet.name }</td>
+              <td>{ planet.rotation_period }</td>
+              <td>{ planet.orbital_period }</td>
+              <td>{ planet.diameter }</td>
+              <td>{ planet.climate }</td>
+              <td>{ planet.gravity }</td>
+              <td>{ planet.terrain }</td>
+              <td>{ planet.surface_water }</td>
+              <td>{ planet.population }</td>
+              <td>{ planet.films }</td>
+              <td>{ planet.created }</td>
+              <td>{ planet.edited }</td>
+              <td>{ planet.url }</td>
             </tr>
           ))}
 
         </tbody>
-
       </table>
     </>
   );
