78888919b194e3ed2a012111d7b8083e1e88673e
josebejohann <josebernardojohann@gmail.com>
2021-03-21 11:44:43 -0300

    Implements a search bar for filter by planets name

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,49 +1,66 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import PlanetsContext from '../contexts/PlanetsContext';
 
 const Table = () => {
   const { data } = useContext(PlanetsContext);
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>NAME</th>
-          <th>ROTATION PERIOD</th>
-          <th>ORBITAL PERIOD</th>
-          <th>DIAMETER</th>
-          <th>CLIMATE</th>
-          <th>GRAVITY</th>
-          <th>TERRAIN</th>
-          <th>SURFACE WATER</th>
-          <th>POPULATION</th>
-          <th>FILMS</th>
-          <th>CREATED AT</th>
-          <th>EDITED AT</th>
-          <th>URL</th>
-        </tr>
-      </thead>
+  const [search, setSearch] = useState('');
 
-      <tbody>
-        {data.map((planet, index) => (
-          <tr key={ index }>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
+  return (
+    <>
+      <input
+        type="text"
+        name="search"
+        value={ search }
+        onChange={ ({ target: { value } }) => setSearch(value) }
+        placeholder="Pesquise por um planeta"
+        data-testid="name-filter"
+      />
+
+      <table>
+        <thead>
+          <tr>
+            <th>NAME</th>
+            <th>ROTATION PERIOD</th>
+            <th>ORBITAL PERIOD</th>
+            <th>DIAMETER</th>
+            <th>CLIMATE</th>
+            <th>GRAVITY</th>
+            <th>TERRAIN</th>
+            <th>SURFACE WATER</th>
+            <th>POPULATION</th>
+            <th>FILMS</th>
+            <th>CREATED AT</th>
+            <th>EDITED AT</th>
+            <th>URL</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          {data.filter((planets) => {
+            if (search === '') return planets;
+
+            return planets.name.toLowerCase().includes(search.toLowerCase());
+          }).map((planet, index) => (
+            <tr key={ index }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
