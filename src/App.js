360ae8dc0bbcdb59b82b14b90cc00ab25935c2db
IzeldaValeriano <zel.valeriano@gmail.com>
2021-03-23 06:19:28 -0300

    requisito 3 e 4

--- src/App.js
+++ src/App.js
@@ -7,33 +7,75 @@ import SWContext from './Context/SWContext';
 
 function App() {
   const [planets, setPlanets] = useState([]);
+  const [menu, setMenu] = useState([]);
   const [filteredPlanets, setFilteredPlanets] = useState([]);
-  const [filters, setFilters] = useState({});
+  const [filters, setFilters] = useState({
+    filterByName: {
+      name: '',
+    },
+    filterByNumericValues: [],
+  });
   const [isFiltered, setIsFiltered] = useState(false);
 
   const getPlanets = () => {
-    fetch('https://swapi.dev/api/planets/')
-      .then((data) => data.json())
-      .then((response) => setPlanets(response.results));
+    if (planets.length < 1) {
+      fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+        .then((data) => data.json())
+        .then((response) => setPlanets(response.results));
+    }
   };
 
   const filterByName = (e) => {
-    if (e.target.value === '') return setIsFiltered(false);
+    if (e.target.value === '' && filters.filterByNumericValues.length < 1) {
+      return setIsFiltered(false);
+    }
     setFilteredPlanets(planets.filter((planet) => planet.name.includes(e.target.value)));
+    setIsFiltered(true);
+    setFilters({ ...filters, filterByName: { name: e.target.value } });
+  };
+
+  const addNumericFilter = (column, comparison, value) => {
+    setFilters({
+      ...filters,
+      filterByNumericValues: [
+        ...filters.filterByNumericValues,
+        { column, comparison, value },
+      ],
+    });
+    switch (comparison) {
+    case 'maior que':
+      setFilteredPlanets(planets.filter((planet) => (
+        Number(planet[column]) > Number(value)
+      )));
+      break;
+    case 'menor que':
+      setFilteredPlanets(planets.filter((planet) => (
+        Number(planet[column]) < Number(value)
+      )));
+      break;
+    case 'igual a':
+      setFilteredPlanets(planets.filter((planet) => (
+        Number(planet[column]) === Number(value)
+      )));
+      break;
+    default:
+      setFilteredPlanets(planets);
+      break;
+    }
     setIsFiltered(true);
   };
   return (
     <SWContext.Provider
       value={ {
         planets,
-        filteredPlanets,
         filters,
+        filteredPlanets,
         isFiltered,
+        menu,
+        setMenu,
         filterByName,
         getPlanets,
-        setFilters,
-        setFilteredPlanets,
-        setIsFiltered,
+        addNumericFilter,
       } }
     >
       <h1>Star Wars Planet Search</h1>
