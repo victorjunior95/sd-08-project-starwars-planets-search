e1cda23e06a63e2d847285bdbf85383835b61a9b
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2021-03-23 02:28:14 -0300

    ate o 4

--- src/Provider.js
+++ src/Provider.js
@@ -4,14 +4,79 @@ import StarWarsContext from './context/StarWarsContext';
 import fetchStarWars from './services';
 
 function Provider({ children }) {
-  const [data, setData] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
+  const [inputName, setInputName] = useState('');
+  const [filter, setFilter] = useState({
+    filterByName: { name: '' },
+  });
+  const [filterOptions, setFilterOptions] = useState({
+    column: 'population',
+    comparison: 'maior que',
+    value: '',
+  });
+  const comparisons = ['maior que', 'menor que', 'igual a'];
+  const [columnSelect, setColumnSelect] = useState(['population',
+    'orbital_period', 'diameter', 'rotation_period', 'surface_water']);
 
   useEffect(() => {
-    fetchStarWars().then((response) => setData(response.results));
+    fetchStarWars().then((response) => {
+      setPlanets(response.results);
+      setFilteredPlanets(response.results);
+    });
   }, []);
 
+  useEffect(() => {
+    const fillAll = planets.filter(({ name }) => name.toLowerCase().includes(inputName));
+    setFilteredPlanets(fillAll);
+  }, [planets, inputName]);
+
+  const handleChange = ({ target }) => {
+    setInputName(target.value);
+  };
+
+  const handleSelect = ({ target: { name, value } }) => {
+    setFilterOptions({
+      ...filterOptions,
+      [name]: value,
+    });
+  };
+
+  const handleFilter = ({ column, comparison, value }) => {
+    const filterEnd = planets.filter((element) => {
+      const columnValue = Number(element[column]);
+      const filterValue = Number(value);
+      if (comparison === 'menor que') {
+        return columnValue < filterValue;
+      }
+      if (comparison === 'maior que') {
+        return columnValue > filterValue;
+      }
+      return columnValue === filterValue;
+    });
+    setFilteredPlanets(filterEnd);
+  };
+
+  const handleClick = () => {
+    const columnFilter = columnSelect.filter((el) => el !== filterOptions.column);
+    setColumnSelect(columnFilter);
+    handleFilter(filterOptions);
+  };
+
   const context = {
-    data,
+    planets,
+    filteredPlanets,
+    setFilteredPlanets,
+    filter,
+    setFilter,
+    handleChange,
+    inputName,
+    columnSelect,
+    handleSelect,
+    comparisons,
+    handleClick,
+    filterOptions,
+    setFilterOptions,
   };
 
   return (
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,62 +1,84 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import StarWarsContext from '../context/StarWarsContext';
+import TableH from './TableH';
 
 function Table() {
-  const { data } = useContext(StarWarsContext);
-  const [fill, setFill] = useState('');
-  return (
-    <>
-      <input
-        type="text"
-        name="filter"
-        value={ fill }
-        onChange={ ({ target }) => setFill(target.value) }
-        data-testid="name-filter"
-      />
-      <table>
-        <thead>
-          <tr>
-            <th>Nome</th>
-            <th>Periodo de rotação </th>
-            <th>Periodo Orbital</th>
-            <th>Diametro</th>
-            <th>Clima</th>
-            <th>Grvidade</th>
-            <th>Terreno</th>
-            <th>Agua na Superficie</th>
-            <th>População</th>
-            <th>Filmes</th>
-            <th>Creado no</th>
-            <th>Editado no</th>
-            <th>URL</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.filter((planets) => {
-            if (fill.length === 0) return planets;
+  const { filteredPlanets, handleChange, inputName, columnSelect,
+    handleSelect, comparisons, handleClick, filterOptions } = useContext(StarWarsContext);
 
-            return planets.name.toLowerCase().includes(fill.toLowerCase());
-          }).map((planet, index) => (
-            <tr key={ index }>
-              <td>{ planet.name }</td>
-              <td>{ planet.rotation_period }</td>
-              <td>{ planet.orbital_period }</td>
-              <td>{ planet.diameter }</td>
-              <td>{ planet.climate }</td>
-              <td>{ planet.gravity }</td>
-              <td>{ planet.terrain }</td>
-              <td>{ planet.surface_water }</td>
-              <td>{ planet.population }</td>
-              <td>{ planet.films }</td>
-              <td>{ planet.created }</td>
-              <td>{ planet.edited }</td>
-              <td>{ planet.url }</td>
+  return (
+    <section>
+      <form>
+        <label htmlFor="Name">
+          Name:
+          <input
+            data-testid="name-filter"
+            type="text"
+            value={ inputName }
+            onChange={ handleChange }
+          />
+        </label>
+
+        <select
+          data-testid="column-filter"
+          name="column"
+          value={ filterOptions.column }
+          onChange={ handleSelect }
+        >
+          {columnSelect.map((el, index) => (
+            <option key={ index }>{el}</option>
+          ))}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          name="comparison"
+          value={ filterOptions.comparison }
+          onChange={ handleSelect }
+        >
+          {comparisons.map((el, index) => (
+            <option key={ index }>{el}</option>
+          ))}
+        </select>
+        <input
+          type="number"
+          name="value"
+          data-testid="value-filter"
+          value={ filterOptions.value }
+          onChange={ handleSelect }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ handleClick }
+        >
+          Filtrar
+        </button>
+
+      </form>
+      <table>
+        <TableH />
+        <tbody>
+          {filteredPlanets.map((el) => (
+            <tr key={ el.name }>
+              <td>{el.name}</td>
+              <td>{el.rotation_period}</td>
+              <td>{el.orbital_period}</td>
+              <td>{el.diameter}</td>
+              <td>{el.climate}</td>
+              <td>{el.gravity}</td>
+              <td>{el.terrain}</td>
+              <td>{el.surface_water}</td>
+              <td>{el.population}</td>
+              <td>{el.films}</td>
+              <td>{el.created}</td>
+              <td>{el.edited}</td>
+              <td>{el.url}</td>
             </tr>
           ))}
-
         </tbody>
       </table>
-    </>
+    </section>
+
   );
 }
 
