042c190878d30e8b1120a1c35141560f2f104f6d
Isabella Paz <bellapaz.iorp@gmail.com>
2021-03-23 08:47:29 -0300

    CSS versao 1

--- src/pages/StarWarsPlanets.js
+++ src/pages/StarWarsPlanets.js
@@ -1,14 +1,20 @@
 import React from 'react';
-// import NumericFilter from '../components/NumericFilter';
+import NumericFilter from '../components/NumericFilter';
 import TextFilter from '../components/TextFilter';
 import PlanetsTable from '../components/PlanetsTable';
+import starWarsLogo from '../images/starWarsLogo.svg';
 
 function starWarsPlanets() {
   return (
     <div>
-      <h1>Star Wars Planets</h1>
+      <img
+        className="star-wars-logo"
+        alt="star wars logo"
+        src={ starWarsLogo }
+      />
+      <h1>Planets</h1>
       <TextFilter />
-      {/* <NumericFilter /> */}
+      <NumericFilter />
       <PlanetsTable />
     </div>
   );
