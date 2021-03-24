2d60a9eb0ba16f76876accbf594c91cb80c418c4
Cleberton <clebertonfgc@gmail.com>
2021-03-24 20:59:39 -0300

    Removendo extesão context chome

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { debugContextDevtool } from 'react-context-devtool';
 import App from './App';
 
-const container = document.getElementById('root');
-
-ReactDOM.render(<App />, container);
-debugContextDevtool(container);
+ReactDOM.render(<App />, document.getElementById('root'));
