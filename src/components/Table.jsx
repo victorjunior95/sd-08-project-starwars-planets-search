0a38bc945cee98a4112c63d7202a4e6fd477490b
Andre Sampaio <andresampaiolm@gmail.com>
2021-03-31 12:46:36 -0300

    Ajustments to pass the tests

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -61,12 +61,11 @@ const StarWarsTable = () => {
             filterObject.handleFilterByNumericValues('column', target.value);
           } }
         >
-          <option selected value="">Select the parameter</option>
-          <option value="population">Population</option>
-          <option value="orbital_period">Orbital period</option>
-          <option value="diameter">Diameter</option>
-          <option value="rotation_period">Rotation period</option>
-          <option value="surface_water">Surface water</option>
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
         </select>
         <select
           data-testid="comparison-filter"
@@ -76,10 +75,9 @@ const StarWarsTable = () => {
             filterObject.handleFilterByNumericValues('comparison', target.value);
           } }
         >
-          <option selected>Select the comparison</option>
-          <option value="maior que">Maior que</option>
-          <option value="menor que">Menor que</option>
-          <option value="igual a">Igual a</option>
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
         </select>
         <InputGroup className="mb-2">
           <FormControl
