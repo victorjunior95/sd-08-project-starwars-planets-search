a5dea628d6fe3a61b50d82585b7329cb81a4017a
Caio Menezes Rocha <caiomzrocha@gmail.com>
2021-03-31 17:04:39 -0300

    [REQUISITO-02] Adiciona input de texto para filtrar os planetas pelo nome.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,29 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Table from '../components/Table';
 import PlanetsContext from '../contexts/PlanetsContext';
 
 function Home() {
-  const { fetchPlanets } = useContext(PlanetsContext);
+  const [isFetching, setIsFetching] = useState(true);
+  const { fetchPlanets, filters, handleFilterByName } = useContext(PlanetsContext);
+  const { name } = filters.filterByName;
 
   useEffect(() => {
     fetchPlanets().then((planets) => planets);
+    setIsFetching(false);
   }, []);
 
   return (
-    <Table />
+    <>
+      <header>
+        <input
+          data-testid="name-filter"
+          type="text"
+          onChange={ (event) => handleFilterByName(event.target.value) }
+          value={ name }
+        />
+      </header>
+      { !isFetching && <Table /> }
+    </>
   );
 }
 
