3e1fd71a67d1296ebb2567bb93c181ffbc059f0a
enicio <jessandro42@gmail.com>
2021-04-09 13:27:46 -0300

    Requisito 5 - Adicionado os arquivos filters

---
+++ src/components/filters.js
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import myContext from '../context/dataContext';
+
+function Filters() {
+  const { filterByPlanetName, setFilterByPlanetName } = useContext(myContext);
+  const { filters: { filterByName,
+    filterByNumericValues } } = filterByPlanetName;
+
+  function removeFilter({ target }) {
+    const filterRemoved = filterByNumericValues
+      .filter((cada) => cada.column !== target.name);
+    setFilterByPlanetName({
+      ...filterByPlanetName,
+      filters: {
+        filterByName,
+        filterByNumericValues: filterRemoved,
+      },
+    });
+  }
+
+  return (
+    <ul>
+      { filterByNumericValues.map((cada) => (
+        <li data-testid="filter" key={ cada.column }>
+          {cada.column}
+          {' '}
+          {cada.comparsion}
+          {' '}
+          {cada.value}
+          <button name={ cada.column } onClick={ removeFilter } type="button">X</button>
+          {' '}
+        </li>))}
+    </ul>
+  );
+}
+
+export default Filters;
