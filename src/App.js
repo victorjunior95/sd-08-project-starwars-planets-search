40f6f343a4eabc7919ba93c7218389c2a70a27f0
ambrosio <matmagamb1@gmail.com>
2021-04-11 18:14:51 -0300

    tudo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import FiltrosAtivos from './components/FiltrosAtivos';
+import Filtro from './components/Filtro';
+import Table from './components/Table';
+import Provider from './context/Provider';
 
 function App() {
   return (
-    <span>Hello, AAApp!</span>
+    <Provider>
+      <Filtro />
+      <FiltrosAtivos />
+      <Table />
+    </Provider>
   );
 }
 
 export default App;
+
+//feito com auxilio de stackoverflow, projeto meu antigo da turma 6, e repositorio de Daniel greco
---
+++ src/components/FiltroNumero.js
@@ -0,0 +1,72 @@
+import React, { useContext, useEffect, useState } from 'react';
+import PlanetsContext from '../context/Context';
+
+export default function FiltroNumero() {
+  const { filters, setFilters, columns, setColumns } = useContext(PlanetsContext);
+
+  const [filterValues, setFilterValues] = useState({});
+
+  const { filtrarPorNumero } = filters;
+  const { column, comparison, value } = filterValues;
+
+  const comparisons = ['maior que', 'menor que', 'igual a'];
+
+  useEffect(() => {
+    setFilterValues({
+      column: columns[0],
+      comparison: 'maior que',
+      value: '0',
+    });
+  }, [columns]);
+
+  const handleChange = ({ target }) => {
+    setFilterValues({
+      ...filterValues,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleFilter = () => {
+    setFilters({
+      ...filters,
+      filtrarPorNumero: [...filtrarPorNumero, filterValues],
+    });
+    setColumns(columns.filter((columnName) => column !== columnName));
+  };
+
+  return (
+    <>
+      <select
+        value={ column }
+        name="column"
+        onChange={ handleChange }
+        data-testid="column-filter"
+      >
+        {columns.map((columnOption) => (
+          <option key={ columnOption }>{columnOption}</option>
+        ))}
+      </select>
+      <select
+        value={ comparison }
+        name="comparison"
+        onChange={ handleChange }
+        data-testid="comparison-filter"
+      >
+        {comparisons.map((comparisonOption) => (
+          <option key={ comparisonOption }>{comparisonOption}</option>
+        ))}
+      </select>
+      <input
+        type="number"
+        value={ value }
+        name="value"
+        data-testid="value-filter"
+        placeholder="0"
+        onChange={ handleChange }
+      />
+      <button type="button" data-testid="button-filter" onClick={ handleFilter }>
+        Filter
+      </button>
+    </>
+  );
+}
