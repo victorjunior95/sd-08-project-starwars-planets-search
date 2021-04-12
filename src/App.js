7b927bb91bb7a267678fe8de35b7a44a9a004af8
Alstent <rafaelasltent@gmail.com>
2021-04-12 18:53:03 +0200

    resolvendo erro

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './App.css';
-import PlanetsProvider from './context/PlanetsProvider';
-import FilterProvider from './context/FilterProvider';
-
-import Header from './components/Header';
+import DataProvider from './context/DataProvider';
 import Table from './components/Table';
-import Filter from './components/Filter';
+import Inputs from './components/Inputs';
 
 function App() {
   return (
-    <PlanetsProvider>
-      <FilterProvider>
-        <Header />
-        <Filter />
+    <DataProvider>
+      <div>
+        <Inputs />
         <Table />
-      </FilterProvider>
-    </PlanetsProvider>
+      </div>
+    </DataProvider>
   );
 }
 
