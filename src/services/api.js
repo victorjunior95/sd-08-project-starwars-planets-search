2813fbedbc541f3c6dd7e5711bea24f9486455d5
antonio arieiro <sfgfffs@gmail.com>
2021-04-12 12:15:04 -0300

    fix destin pages

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const api = {
   get: async (route) => {
-    const result = await fetch(`https://swapi-trybe.herokuapp.com/api${route}`);
+    const result = await fetch(`htps://swapi-trybe.herokuapp.com/api${route}`);
     const json = await result.json();
 
     return json;
